dbbbd9914a6a323552ad95641b6cd7e0d6f718b1
topfm <mtopf@BaseByte.peplab.microbialsciences.wisc.edu>
2024-05-21 10:41:41 -0500

    editing for use on basebyte with piotii

--- snpEff.sh
+++ snpEff.sh
@@ -6,9 +6,12 @@ Rscript poolER/csvToVCF.R $1
 
 # Use the output file of the R script as input for the Java script
 # Assuming $f contains a filename with .csv extension
-f_without_extension="${1%.csv}"
-java -Xmx8g -jar ~/programs/snpEff/snpEff.jar Gardnerella_piotii "${f_without_extension}.vcf" > snpEff.tsv
+
+file="${1%.csv}"
+output="${file/_popoolation_withAnnot/_snpEff.tsv}"
+
+java -Xmx8g -jar /opt/PepPrograms/snpEff_v5.0/snpEff.jar JNFY15 "$file.vcf" > $output
 
 
 #run R script to combine 
-Rscript poolER/combine.R $1
+Rscript poolER/combine.R $1 $output
